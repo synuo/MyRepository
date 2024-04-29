41db006fca3e3423f07bbcb03c3d031fc3bbed99
synuo <127924043+synuo@users.noreply.github.com>
2024-04-29 22:34:01 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -3,9 +3,26 @@
 using namespace std;
 
 int N, M;
-int arr[100001];
+int a[100005];
 
-int BinarySearch(int arr[], int size, int target);
+int binarysearch(int target) {
+	int st = 0;
+	int en = N - 1;
+
+	while (st <= en) {
+		int mid = (st + en) / 2;
+		if (target < a[mid])
+			en = mid - 1;
+		else if (target > a[mid])
+			st = mid + 1;
+		else
+			return 1;
+	}
+
+	return 0;
+
+
+}
 int main() {
 
 	ios::sync_with_stdio(0);
@@ -13,33 +30,15 @@ int main() {
 
 	cin >> N;
 	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
+		cin >> a[i];
 	}
 
-	sort(arr, arr + N);
+	sort(a, a + N);
 
 	cin >> M;
 	for (int i = 0; i < M; i++) {
-		int x; cin >> x;
-		// cout << binary_search(arr, arr + N, x) << "\n";
-		cout << BinarySearch(arr, N, x) << "\n";
-	}
-
-	return 0;
-}
-int BinarySearch(int arr[], int size, int target) {
-	int left = 0;
-	int right = size - 1;
-	int mid = 0;
-
-	while (left <= right) {
-		mid = (left + right) / 2;
-		if (target < arr[mid])
-			right = mid - 1;
-		else if (target > arr[mid])
-			left = mid + 1;
-		else
-			return 1;
+		int t; cin >> t;
+		cout << binarysearch(t) << "\n";
 	}
 
 	return 0;
